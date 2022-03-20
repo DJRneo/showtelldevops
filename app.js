2ed23dbe371d28edb2423c8dd812412ce5d75e25
DJRneo <98994092+DJRneo@users.noreply.github.com>
2022-03-21 02:03:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 app.listen(8080, () => { console.log('Server running on port 3000') })
 
-app.get("/", (req, res) => { res.send(['Hello! This is version 0.18']) })
+app.get('/', (req, res) => { res.send(['Hello! This is version 0.18']) })
