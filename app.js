54f36db4e8405aa9b7b717b58865577a8c9af8ca
DJRneo <98994092+DJRneo@users.noreply.github.com>
2022-03-21 01:14:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@
 document.getElementById("demo").innerHTML = "My First JavaScript";
 const test = 'I am a test';
 console.log(`Test: ${test}`);
-const test = 'Another one.';
+const test = 'Another one.';;;
 </script>
